9c98e885f02d8a71ac956b1d4c30c816bbd9da12
Jun_haoer <69341062+Junhaoer@users.noreply.github.com>
2026-07-24 02:51:25 +0800

    feat: add SettingsFontFamilyOption model

---
+++ Launcher.App/ViewModels/Settings/SettingsFontFamilyOption.cs
@@ -0,0 +1,33 @@
+/*
+ * BlockHelm Launcher
+ * Copyright (C) 2026 Quan Zhou
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+namespace Launcher.App.ViewModels.Settings;
+
+public sealed class SettingsFontFamilyOption
+{
+    public SettingsFontFamilyOption(string id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+
+    public string Id { get; }
+
+    public string Title { get; }
+}
